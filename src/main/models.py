0f11ccc53490a04e94b0bb323e10f0a91cca8a51
Mikita Yeudakimau <nikevdokimovv@gmail.com>
2022-11-08 23:41:40 +0300

    models added

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Office(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Office {self.name}, {self.address}"
+
+
+class Room(models.Model):
+    number = models.CharField(max_length=15)
+    office = models.ForeignKey(Office, on_delete=models.CASCADE, related_name="office")
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f"Room #{self.number}, {self.office}"
+
+
+class Reservation(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="room_reservation")
+    data = models.DateField()
+    reserved_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="user")
+
+
+class Rest(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="room_rest")
+    rest = models.IntegerField()
+    date = models.DateField()
